a0282f675ec11aea0a69ca6a50fcbc4cd31a0ad5
Rohith Reddy Illuri <rohith.illuri@gmail.com>
2024-08-12 15:15:16 -0500

    mongo-connect

--- daily-tracker-service/src/main/java/com/daily/tracker/service/collection/Person.java
+++ daily-tracker-service/src/main/java/com/daily/tracker/service/collection/Person.java
@@ -5,7 +5,7 @@ package com.daily.tracker.service.collection;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
-import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
